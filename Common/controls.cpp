8397f63270242feafa3bbf042ccd178ed2c7cc31
DinaAdib <dina962008@hotmail.com>
2017-12-21 18:32:49 +0200

    Add files via upload

--- Common/controls.cpp
+++ Common/controls.cpp
@@ -1,37 +1,40 @@
-// Include GLFW
 #include <GL/glfw.h>
 #include <iostream>
-// Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include "controls.hpp"
 using namespace glm;
 using namespace std;
 
-#include "controls.hpp"
 float rightFactor;
-float fuel = 10000;
-bool fuelRunOut= false;
+int fuel = 1000000;
+int GameTime = 1200000;
+
+bool fuelRunOut = false;
 glm::mat4 ViewMatrix;
 glm::mat4 ProjectionMatrix;
 glm::vec3 position = glm::vec3(0, 0, 5);
 glm::vec3 SSPosition = glm::vec3(0, 0.5, 8);
-glm::mat4 getViewMatrix() {
-	return ViewMatrix;
+
+glm::mat4 getViewMatrix()
+{
+    return ViewMatrix;
 }
-glm::mat4 getProjectionMatrix() {
-	return ProjectionMatrix;
+glm::mat4 getProjectionMatrix()
+{
+    return ProjectionMatrix;
 }
 
 glm::vec3 getCameraPosition()
 {
-	return position;
+    return position;
 }
 
 glm::vec3 getSSPosition()
 {
-   // SSPosition.x = position.x;
-    SSPosition.y = position.y-0.5f;
-    SSPosition.z = position.z-3.0f;
+    //SSPosition.x = position.x;
+    SSPosition.y = position.y - 0.5f;
+    SSPosition.z = position.z - 3.0f;
     return SSPosition;
 }
 
@@ -42,104 +45,172 @@ float verticalAngle = 0.0f;
 // Initial Field of View
 float initialFoV = 45.0f;
 
-float speed = 40.0f; // 3 units / second
+float horizontalSpeed, verticalSpeed, forwardSpeed = 40.0f; // 3 units / second
 float mouseSpeed = 0.005f;
 bool wasPressed = false;
-float factor = 0.0f;
+float factor = 0.0f, tunnelFactor = 1.0f;
+bool firstTime = true;
+int xpos, ypos;
 
-void computeMatricesFromInputs() {
+void computeMatricesFromInputs()
+{
+    // glfwGetTime is called only once, the first time this function is called
+    static double lastTime = glfwGetTime();
 
-	// glfwGetTime is called only once, the first time this function is called
-	static double lastTime = glfwGetTime();
+    // Compute time difference between current and last frame
+    double currentTime = glfwGetTime();
+    float deltaTime = float(currentTime - lastTime);
 
-	// Compute time difference between current and last frame
-	double currentTime = glfwGetTime();
-	float deltaTime = float(currentTime - lastTime);
+    // Get mouse position
+    if(firstTime){
+        firstTime = false;
+        glfwGetMousePos(&xpos, &ypos);
+    }
 
-	// Get mouse position
-	int xpos, ypos;
-	glfwGetMousePos(&xpos, &ypos);
+    // Reset mouse position for next frame
+    // EDIT : Doesn't work on Mac OS, hence the code below is a bit different from the website's
+    //glfwSetMousePos(1024/2, 768/2);
 
-	// Reset mouse position for next frame
-	// EDIT : Doesn't work on Mac OS, hence the code below is a bit different from the website's
-	//glfwSetMousePos(1024/2, 768/2);
+    // Compute new orientation
+    horizontalAngle = 3.14f + mouseSpeed * float(1024 / 2 - xpos);
+    verticalAngle = mouseSpeed * float(768 / 2 - ypos);
 
-	// Compute new orientation
-	horizontalAngle = 3.14f + mouseSpeed * float(1024 / 2 - xpos);
-	verticalAngle = mouseSpeed * float(768 / 2 - ypos);
-
-	// Direction : Spherical coordinates to Cartesian coordinates conversion
-	glm::vec3 direction(
+    // Direction : Spherical coordinates to Cartesian coordinates conversion
+    glm::vec3 direction
+    (
         0,
-		sin(verticalAngle),
-		cos(verticalAngle) * cos(horizontalAngle)
-		);
+        sin(verticalAngle),
+        cos(verticalAngle) * cos(horizontalAngle)
+    );
 
-	// Right vector
-	glm::vec3 right = glm::vec3(
-		sin(horizontalAngle - 3.14f / 2.0f),
-		0,
-		cos(horizontalAngle - 3.14f / 2.0f)
-		);
+    // Right vector
+    glm::vec3 right = glm::vec3
+    (
+        sin(horizontalAngle - 3.14f / 2.0f),
+        0,
+        cos(horizontalAngle - 3.14f / 2.0f)
+    );
 
-	// Up vector
-	glm::vec3 up = glm::cross(right, direction);
+    // Up vector
+    glm::vec3 up = glm::cross(right, direction);
     //if was pressed last time, increment and multiply by speed as a range from 0 to 1, else start over.
 
-	// Move forward
-	if (glfwGetKey(GLFW_KEY_UP) == GLFW_PRESS) {
-        position += up * deltaTime * speed;
-	}
-    if (glfwGetKey(GLFW_KEY_SPACE) == GLFW_PRESS) {
 
-        if(wasPressed and factor < 1) {
-            factor+=0.0004;
+    if (glfwGetKey(GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        if (wasPressed && factor < 1)
+        {
+            factor += 0.0004;
         }
-        else if(!wasPressed) factor = 0.0;
+        else if (!wasPressed)
+        {
+            factor = 0.0;
+        }
         wasPressed = true;
-        if(!fuelRunOut)
-        position += direction * deltaTime * speed * factor;
+        if (!fuelRunOut)
+        {
+            position += direction * deltaTime * speed * factor*tunnelFactor;
+        }
     }
-    if(glfwGetKey(GLFW_KEY_SPACE) != GLFW_PRESS && factor > 0){
-        factor-=0.0001;
-         if(!fuelRunOut)
-        position += direction * deltaTime * speed * factor;
+    if (glfwGetKey(GLFW_KEY_SPACE) != GLFW_PRESS && factor > 0)
+    {
+        factor -= 0.0001;
+        if (!fuelRunOut)
+        {
+            position += direction * deltaTime * speed * factor*tunnelFactor;
+        }
     }
 
-    fuel-=factor;
-    if(fuel <= 0) {
-        cout<<"run out of fuel";fuelRunOut = true;
+    if (fuel <= 0)
+    {
+        //cout << "run out of fuel";
+        fuelRunOut = true;
     }
-	// Move backward
-	if (glfwGetKey(GLFW_KEY_DOWN) == GLFW_PRESS) {
-        position -= up * deltaTime * speed;
-	}
-	// Strafe right
-	if (glfwGetKey(GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        if(rightFactor <= 15){
-            rightFactor += 0.1;
+    // Move Up
+    if (glfwGetKey(GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        position += up * deltaTime * speed*0.4f;
+    }
+    // Move Down
+    if (glfwGetKey(GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        position -= up * deltaTime * speed*0.4f;
+    }
+    // Strafe right
+    if (glfwGetKey(GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        if (SSPosition.x <= 1)
+        {
             SSPosition += right * deltaTime * speed*0.2f;
         }
-	}
-	// Strafe left
-	if (glfwGetKey(GLFW_KEY_LEFT) == GLFW_PRESS) {
-        if(rightFactor >=-13){
-            rightFactor-=0.1;
-           SSPosition -= right * deltaTime * speed*0.2f;
+    }
+    // Strafe left
+    if (glfwGetKey(GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+        if (SSPosition.x >= -1)
+        {
+            SSPosition -= right * deltaTime * speed*0.2f;
         }
-	}
+    }
 
-	float FoV = initialFoV - 5 * glfwGetMouseWheel();
+    float FoV = initialFoV - 5 * glfwGetMouseWheel();
 
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	ViewMatrix = glm::lookAt(
-		position,           // Camera is here
-		position + direction, // and looks here : at the same position, plus "direction"
-		up                  // Head is up (set to 0,-1,0 to look upside-down)
-		);
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Camera matrix
+    ViewMatrix = glm::lookAt
+    (
+        position,           // Camera is here
+        position + direction, // and looks here : at the same position, plus "direction"
+        up                  // Head is up (set to 0,-1,0 to look upside-down)
+    );
 
-	// For the next frame, the "last time" will be "now"
-	lastTime = currentTime;
+    // For the next frame, the "last time" will be "now"
+    lastTime = currentTime;
+}
+
+void incrementSpeed(int amount)
+{
+    speed += amount;
+}
+void decrementSpeed(int amount)
+{
+    speed -= amount;
+}
+void speedUp(int amount){
+    factor+=amount;
+}
+
+void speedDown(int amount){
+    factor-=amount;
+}
+
+void incrementFuel(int amount)
+{
+    fuel += amount;
+}
+void decrementFuel()
+{
+    fuel -= factor;
+}
+int getFuelLeft()
+{
+    return fuel;
+}
+int getLeftTime()
+{
+    return GameTime;
+}
+void incrementTime(int sec)
+{
+    GameTime += sec;
+}
+void decrementTime(int sec)
+{
+    GameTime -= sec;
+}
+void EndGame()
+{
+    fuel = 0;
+    GameTime = 0;
 }
--- Common/controls.hpp
+++ Common/controls.hpp
@@ -5,6 +5,18 @@ void computeMatricesFromInputs();
 glm::mat4 getViewMatrix();
 glm::mat4 getProjectionMatrix();
 glm::vec3 getCameraPosition();
-
 glm::vec3 getSSPosition();
+
+void incrementSpeed(int amount);
+void decrementSpeed(int amount);
+void incrementFuel(int amount);
+void decrementFuel();
+int getFuelLeft();
+int getLeftTime();
+void incrementTime(int sec);
+void decrementTime(int sec);
+void speedUp(int sec);
+void speedDown(int amount);
+void EndGame();
+
 #endif
